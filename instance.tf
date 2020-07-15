45c3f145d34bfba7195002f551fc6d526af61aa5
Ezzmo <moezzeddin@gmail.com>
2020-07-15 15:01:10 +0100

    selected AMIs

--- instance.tf
+++ instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "front" {
   # ubuntu 18.04
-  ami = "ami-0c30afcb7ab02233d"
+  ami = "ami-0a63f96e85105c6d3"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.default.id}"
+  subnet_id = "${aws_subnet.public.id}"
   vpc_security_group_ids = ["${aws_security_group.front.id}"]
   associate_public_ip_address = true
   key_name = "PracticeKey"
@@ -11,20 +11,16 @@ resource "aws_instance" "front" {
       type = "ssh"
       host = "${self.public_ip}"
       user = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
+      private_key = "${file("~/.ssh/PracticeKey.pem")}"
     }
-    inline = [
-      "echo hi",
-      "hostname"
-    ]
   }
 }
 
 resource "aws_instance" "database" {
   # ubuntu 18.04
-  ami = "ami-0c30afcb7ab02233d"
+  ami = "ami-0a63f96e85105c6d3"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.default.id}"
+  subnet_id = "${aws_subnet.private.id}"
   vpc_security_group_ids = ["${aws_security_group..id}"]
   associate_public_ip_address = true
   key_name = "PracticeKey"
@@ -33,12 +29,8 @@ resource "aws_instance" "database" {
       type = "ssh"
       host = "${self.public_ip}"
       user = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
+      private_key = "${file("~/.ssh/PracticeKey.pem")}"
     }
-    inline = [
-      "echo hi",
-      "hostname"
-    ]
   }
 }
 
